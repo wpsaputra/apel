ce991647633ef568d27798a3d7d4e58303a89607
wayan.saputra <wayan.saputra@bps.go.id>
2019-05-17 15:26:01 +0800

    fix nav selected keys

--- src/App.js
+++ src/App.js
@@ -16,79 +16,84 @@ class App extends React.Component {
   };
 
   onCollapse = collapsed => {
-    console.log(collapsed);
+    // console.log(collapsed);
+    // console.log(location);
     this.setState({ collapsed });
   };
 
   render() {
     return (
       <Router>
+        <Route render={(props) => {
+          // console.log(props.location)
+          return (
+            <Layout style={{ minHeight: '100vh' }}>
+              <Sider breakpoint="lg" collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
+                <div className="logo" />
+                <Menu theme="dark" selectedKeys={[props.location.pathname]} mode="inline">
+                  <Menu.Item key="/">
+                    <Icon type="pie-chart" />
+                    <span>Home</span>
+                    <NavLink to='/' exact={true}> Home </NavLink>
+                  </Menu.Item>
+                  <Menu.Item key="/login">
+                    <Icon type="desktop" />
+                    <span>Login</span>
+                    <NavLink to='/login'> Home </NavLink>
+                  </Menu.Item>
+                  <SubMenu
+                    key="sub1"
+                    title={
+                      <span>
+                        <Icon type="user" />
+                        <span>User</span>
+                      </span>
+                    }
+                  >
+                    <Menu.Item key="3">Tom</Menu.Item>
+                    <Menu.Item key="4">Bill</Menu.Item>
+                    <Menu.Item key="5">Alex</Menu.Item>
+                  </SubMenu>
+                  <SubMenu
+                    key="sub2"
+                    title={
+                      <span>
+                        <Icon type="team" />
+                        <span>Team</span>
+                      </span>
+                    }
+                  >
+                    <Menu.Item key="6">Team 1</Menu.Item>
+                    <Menu.Item key="8">Team 2</Menu.Item>
+                  </SubMenu>
+                  <Menu.Item key="9">
+                    <Icon type="file" />
+                    <span>File</span>
+                  </Menu.Item>
+                </Menu>
+              </Sider>
+              <Layout>
+                <Header style={{ background: '#fff', padding: 0 }} />
+                <Content style={{ margin: '0 16px' }}>
+                  <Breadcrumb style={{ margin: '16px 0' }}>
+                    <Breadcrumb.Item>User</Breadcrumb.Item>
+                    <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                  </Breadcrumb>
+                  {/* <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>Bill is a cat.</div> */}
+                  <Route exact path="/" component={Home} />
+                  <Route path="/login" component={Login} />
 
-        <Layout style={{ minHeight: '100vh' }}>
-          <Sider breakpoint="lg" collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
-            <div className="logo" />
-            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-              <Menu.Item key="1">
-                <Icon type="pie-chart" />
-                <span>Option 1</span>
-                {/* <Link to="/" /> */}
-                <NavLink to='/' activeClassName="ant-menu-item-selected" exact={true}> Home </NavLink>
-              </Menu.Item>
-              <Menu.Item key="2">
-                <Icon type="desktop" />
-                <span>Option 2</span>
-                {/* <Link to="/login" /> */}
-                <NavLink to='/login' activeStyle={{color: 'red'}}> Home </NavLink>
-              </Menu.Item>
-              <SubMenu
-                key="sub1"
-                title={
-                  <span>
-                    <Icon type="user" />
-                    <span>User</span>
-                  </span>
-                }
-              >
-                <Menu.Item key="3">Tom</Menu.Item>
-                <Menu.Item key="4">Bill</Menu.Item>
-                <Menu.Item key="5">Alex</Menu.Item>
-              </SubMenu>
-              <SubMenu
-                key="sub2"
-                title={
-                  <span>
-                    <Icon type="team" />
-                    <span>Team</span>
-                  </span>
-                }
-              >
-                <Menu.Item key="6">Team 1</Menu.Item>
-                <Menu.Item key="8">Team 2</Menu.Item>
-              </SubMenu>
-              <Menu.Item key="9">
-                <Icon type="file" />
-                <span>File</span>
-              </Menu.Item>
-            </Menu>
-          </Sider>
-          <Layout>
-            <Header style={{ background: '#fff', padding: 0 }} />
-            <Content style={{ margin: '0 16px' }}>
-              <Breadcrumb style={{ margin: '16px 0' }}>
-                <Breadcrumb.Item>User</Breadcrumb.Item>
-                <Breadcrumb.Item>Bill</Breadcrumb.Item>
-              </Breadcrumb>
-              {/* <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>Bill is a cat.</div> */}
-              <Route exact path="/" component={Home} />
-              <Route path="/login" component={Login} />
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>Apel ©2019 Created by Developers</Footer>
+              </Layout>
+            </Layout>
+          )
+        }} />
 
-            </Content>
-            <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-          </Layout>
-        </Layout>
       </Router>
     );
   }
 }
 
+// export default App;
 export default App;
