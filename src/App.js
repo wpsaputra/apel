4615da4b8aa2b036e3ba45b7e84ec2758f690ae4
wpsaputra <wayanps93@gmail.com>
2019-05-20 00:17:52 +0800

    add logo side menu

--- src/App.js
+++ src/App.js
@@ -29,62 +29,63 @@ class App extends React.Component {
           return (
             <Layout style={{ minHeight: '100vh' }}>
               <Sider breakpoint="lg" collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
-                <div className="logo" />
+                <div className="logo"> <Icon type="apple" theme="filled" style={{ fontSize: '40px', color: 'grey' }}/> </div>
                 <Menu theme="dark" selectedKeys={[props.location.pathname]} mode="inline">
                   <Menu.Item key="/">
-                    <Icon type="pie-chart" />
+                    <Icon type="home" />
                     <span>Home</span>
                     <NavLink to='/' exact={true}> Home </NavLink>
                   </Menu.Item>
-                  <Menu.Item key="/login">
-                    <Icon type="desktop" />
-                    <span>Login</span>
-                    <NavLink to='/login'> Home </NavLink>
+                  <Menu.Item key="/penilaian">
+                    <Icon type="calculator" />
+                    <span>Penilaian</span>
+                    <NavLink to='/penilaian'> Penilaian </NavLink>
                   </Menu.Item>
                   <SubMenu
                     key="sub1"
                     title={
                       <span>
-                        <Icon type="user" />
-                        <span>User</span>
+                        <Icon type="line-chart" />
+                        <span>Rekapitulasi</span>
                       </span>
                     }
                   >
-                    <Menu.Item key="3">Tom</Menu.Item>
-                    <Menu.Item key="4">Bill</Menu.Item>
-                    <Menu.Item key="5">Alex</Menu.Item>
+                    <Menu.Item key="3">Per Pegawai</Menu.Item>
+                    <Menu.Item key="4">Bulanan</Menu.Item>
+                    {/* <Menu.Item key="5">Alex</Menu.Item> */}
                   </SubMenu>
                   <SubMenu
                     key="sub2"
                     title={
                       <span>
                         <Icon type="team" />
-                        <span>Team</span>
+                        <span>Registrasi</span>
                       </span>
                     }
                   >
-                    <Menu.Item key="6">Team 1</Menu.Item>
-                    <Menu.Item key="8">Team 2</Menu.Item>
+                    <Menu.Item key="6">Administrator</Menu.Item>
+                    {/* <Menu.Item key="8">Team 2</Menu.Item> */}
                   </SubMenu>
-                  <Menu.Item key="9">
+                  {/* <Menu.Item key="9">
                     <Icon type="file" />
                     <span>File</span>
-                  </Menu.Item>
+                  </Menu.Item> */}
                 </Menu>
               </Sider>
               <Layout>
-                <Header style={{ background: '#fff', padding: 0 }} />
+                <Header style={{ background: '#fff', padding: 0 }}>
+                </Header>
                 <Content style={{ margin: '0 16px' }}>
                   <Breadcrumb style={{ margin: '16px 0' }}>
                     <Breadcrumb.Item>User</Breadcrumb.Item>
                     <Breadcrumb.Item>Bill</Breadcrumb.Item>
                   </Breadcrumb>
-                  {/* <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>Bill is a cat.</div> */}
+                  <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>Bill is a cat.</div>
                   <Route exact path="/" component={Home} />
                   <Route path="/login" component={Login} />
 
                 </Content>
-                <Footer style={{ textAlign: 'center' }}>Apel ©2019 Created by Developers</Footer>
+                <Footer style={{ textAlign: 'center' }}>Copyright ©2019 Created by Developers</Footer>
               </Layout>
             </Layout>
           )
