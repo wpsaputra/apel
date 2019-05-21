3ad82418ddc3a5c099c41cbe3fcad1cdd2977c19
wayan.saputra <wayan.saputra@bps.go.id>
2019-05-21 10:27:38 +0800

    add axios on penilaian

--- src/pages/penilaian.jsx
+++ src/pages/penilaian.jsx
@@ -2,218 +2,115 @@ import React, { Component } from 'react'
 import { Card, DatePicker, Table, Divider, Tag, notification } from 'antd'
 
 const { MonthPicker } = DatePicker;
-const openNotification = () => {
-    notification.open({
-      message: 'Perubahan Tabel',
-      description:
-        'Konten tabel berhasil dirubah',
-      onClick: () => {
-        console.log('Notification Clicked!');
-      },
-    });
-  };
-
-function onChange(date, dateString) {
-    console.log(date, dateString);
-    openNotification();
-}
-
-const columns = [
-    {
-        title: 'Nama Pegawai',
-        dataIndex: 'name',
-        key: 'name',
-        render: text => <a href="javascript:;">{text}</a>,
-    },
-    {
-        title: 'Skor Realisasi Pekerjaan',
-        dataIndex: 'age',
-        key: 'age',
-    },
-    {
-        title: 'Skor Ketepatan Waktu',
-        dataIndex: 'tepat',
-        key: 'tepat',
-    },
-    {
-        title: 'Skor Daily Activity',
-        dataIndex: 'address',
-        key: 'address',
-    },
-    {
-        title: 'Skor TL & PSW',
-        dataIndex: 'tl',
-        key: 'tl',
-    },
-    {
-        title: 'Nilai CKP R',
-        dataIndex: 'ckpr',
-        key: 'ckpr',
-    },
-    {
-        title: 'Status',
-        key: 'tags',
-        dataIndex: 'tags',
-        render: tags => (
-            <span>
-                {tags.map(tag => {
-                    let color = tag.length > 5 ? 'geekblue' : 'green';
-                    if (tag === 'incomplete') {
-                        color = 'volcano';
-                    }else{
-                        color = 'green';
-                    }
-                    return (
-                        <Tag color={color} key={tag}>
-                            {tag.toUpperCase()}
-                        </Tag>
-                    );
-                })}
-            </span>
-        ),
-    },
-    {
-        title: 'Action',
-        key: 'action',
-        render: (text, record) => (
-            <span>
-                <a href="javascript:;">Edit</a>
-                <Divider type="vertical" />
-                <a href="javascript:;">Delete</a>
-            </span>
-        ),
-    },
-];
-
-const data = [
-    {
-        key: '1',
-        name: 'John Brown',
-        age: 90,
-        tepat: 90,
-        address: 100,
-        tl: 100,
-        ckpr: 95,
-        tags: ['complete'],
-    },
-    {
-        key: '2',
-        name: 'Jim Green',
-        age: 99,
-        tepat: 99,
-        address: 95,
-        tl: 95,
-        ckpr: 90,
-        tags: ['complete'],
-    },
-    {
-        key: '3',
-        name: 'Joe Black',
-        age: 100,
-        tepat: "",
-        address: 90,
-        tl: 90,
-        ckpr: "",
-        tags: ['incomplete'],
-    },
-    {
-        key: '1',
-        name: 'John Brown',
-        age: 90,
-        tepat: 90,
-        address: 100,
-        tl: 100,
-        ckpr: 95,
-        tags: ['complete'],
-    },
-    {
-        key: '2',
-        name: 'Jim Green',
-        age: 99,
-        tepat: 99,
-        address: 95,
-        tl: 95,
-        ckpr: 90,
-        tags: ['complete'],
-    },
-    {
-        key: '3',
-        name: 'Joe Black',
-        age: 100,
-        tepat: "",
-        address: 90,
-        tl: 90,
-        ckpr: "",
-        tags: ['incomplete'],
-    },
-    {
-        key: '1',
-        name: 'John Brown',
-        age: 90,
-        tepat: 90,
-        address: 100,
-        tl: 100,
-        ckpr: 95,
-        tags: ['complete'],
-    },
-    {
-        key: '2',
-        name: 'Jim Green',
-        age: 99,
-        tepat: 99,
-        address: 95,
-        tl: 95,
-        ckpr: 90,
-        tags: ['complete'],
-    },
-    {
-        key: '3',
-        name: 'Joe Black',
-        age: 100,
-        tepat: "",
-        address: 90,
-        tl: 90,
-        ckpr: "",
-        tags: ['incomplete'],
-    },
-    {
-        key: '1',
-        name: 'John Brown',
-        age: 90,
-        tepat: 90,
-        address: 100,
-        tl: 100,
-        ckpr: 95,
-        tags: ['complete'],
-    },
-    {
-        key: '2',
-        name: 'Jim Green',
-        age: 99,
-        tepat: 99,
-        address: 95,
-        tl: 95,
-        ckpr: 90,
-        tags: ['complete'],
-    },
-    {
-        key: '3',
-        name: 'Joe Black',
-        age: 100,
-        tepat: "",
-        address: 90,
-        tl: 90,
-        ckpr: "",
-        tags: ['incomplete'],
-    },
-];
+const axios = require('axios');
 
 export class penilaian extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            columns: [
+                {
+                    title: 'Nama Pegawai',
+                    dataIndex: 'niplama',
+                    key: 'niplama',
+                    render: text => <a href="javascript:;">{text}</a>,
+                },
+                {
+                    title: 'Skor Realisasi Pekerjaan',
+                    dataIndex: 'skor_realisasi_pekerjaan',
+                    key: 'skor_realisasi_pekerjaan',
+                },
+                {
+                    title: 'Skor Ketepatan Waktu',
+                    dataIndex: 'skor_ketepatan_waktu',
+                    key: 'skor_ketepatan_waktu',
+                },
+                {
+                    title: 'Skor Daily Activity',
+                    dataIndex: 'skor_daily_activity',
+                    key: 'skor_daily_activity',
+                },
+                {
+                    title: 'Skor TL & PSW',
+                    dataIndex: 'skor_tl_psw',
+                    key: 'skor_tl_psw',
+                },
+                {
+                    title: 'Nilai CKP R',
+                    dataIndex: 'nilai_ckp_r',
+                    key: 'nilai_ckp_r',
+                },
+                {
+                    title: 'Status',
+                    key: 'status',
+                    dataIndex: 'status',
+                    render: status => (
+                        <span>
+                            <Tag color={status==='incomplete'? 'volcano':'green'}>
+                                {status.toUpperCase()}
+                            </Tag>
+                        </span>
+                    ),
+                },
+                {
+                    title: 'Action',
+                    key: 'action',
+                    render: (text, record) => (
+                        <span>
+                            <a href="javascript:;">Edit</a>
+                            <Divider type="vertical" />
+                            <a href="javascript:;">Delete</a>
+                        </span>
+                    ),
+                },
+            ],
+            data: []
+        };
+        this.openNotification = this.openNotification.bind(this);
+        this.onChange = this.onChange.bind(this);
+        this.fetchData = this.fetchData.bind(this);
+    }
+
+    openNotification() {
+        notification.open({
+            message: 'Perubahan Tabel',
+            description:
+                'Konten tabel berhasil dirubah',
+            onClick: () => {
+                console.log('Notification Clicked!');
+            },
+        });
+    }
+
+    onChange(date, dateString) {
+        // console.log(date, dateString);
+        console.log(dateString);
+        this.fetchData();
+        this.openNotification();
+    }
+
+    fetchData() {
+        var self = this;
+        axios.get('http://localhost/api.php/records/penilaian')
+            .then(function (response) {
+                // handle success
+                console.log(response.data.records);
+                self.setState({data: response.data.records});
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .finally(function () {
+                // always executed
+            });
+    }
+
     render() {
         return (
             <Card>
-                <MonthPicker style={{ marginBottom: "10px" }} onChange={onChange} placeholder="Select month" />
-                <Table columns={columns} dataSource={data} />
+                <MonthPicker style={{ marginBottom: "10px" }} onChange={this.onChange} placeholder="Select month" />
+                <Table columns={this.state.columns} dataSource={this.state.data} />
             </Card>
         )
     }
