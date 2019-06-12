fd81a562065456f23429424eb5ea57627bf2dd9c
wayan.saputra <wayan.saputra@bps.go.id>
2019-06-12 10:21:44 +0800

    fix pegawai api

--- src/api/pegawai.php
+++ src/api/pegawai.php
@@ -89,7 +89,7 @@ class Pegawai{
         $nerwilis=$this->check_92850();
         $ipds=$this->check_92860();
         $querys="SELECT*FROM master_pegawai JOIN autentifikasi ON master_pegawai.niplama=autentifikasi.niplama
-        WHERE master_pegawai.id_satker=".$this->session->userdata('satker')." AND (";
+        WHERE master_pegawai.id_satker=".$this->id_satker." AND (";
         if($tu < 1){
             $querys.='master_pegawai.id_org="92810" OR ';
         }
@@ -110,109 +110,259 @@ class Pegawai{
         }
     
         $querys.='master_pegawai.id_org="92870" OR autentifikasi.id_level="3")';
-        $this->db->query($querys);
-        $query = $this->db->query($querys);
+        // $this->db->query($querys);
+        // $query = $this->db->query($querys);
     
-    return $query->result();
+        // return $query->result();
+
+        $result = "Error get eselon 4 KSK";
+        try {
+            $conn = new PDO("mysql:host=$this->servername;dbname=$this->dbname_ckp", $this->username_ckp, $this->password_ckp);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $stmt = $conn->prepare($querys);
+            $stmt->execute();
+            $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $result = $stmt->fetchAll();
+            // $result = $stmt;
+        
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+        return $result;
+
     }
     
     function check_92810()
     {
-        $this->db->from($this->table);
-        $this->db->join($this->autentifikasi, 'master_pegawai.niplama=autentifikasi.niplama');
-        $this->db->where('autentifikasi.id_level =', '3');
-        $this->db->where('master_pegawai.id_satker', $this->session->userdata('satker'));
-        $this->db->where('id_org', '92810');
-        $query = $this->db->get();
+        // $this->db->from($this->table);
+        // $this->db->join($this->autentifikasi, 'master_pegawai.niplama=autentifikasi.niplama');
+        // $this->db->where('autentifikasi.id_level =', '3');
+        // $this->db->where('id_org', '92810');
+        // $this->db->where('master_pegawai.id_satker', $this->session->userdata('satker'));
+        // $query = $this->db->get();
     
-        return $query->num_rows();
+        // return $query->num_rows();
+
+        $result = "Error cek 92810";
+        try {
+            $conn = new PDO("mysql:host=$this->servername;dbname=$this->dbname_ckp", $this->username_ckp, $this->password_ckp);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $stmt = $conn->prepare("SELECT COUNT(*) FROM master_pegawai JOIN autentifikasi ON master_pegawai.niplama=autentifikasi.niplama WHERE autentifikasi.id_level=3
+                AND id_org='92810' AND master_pegawai.id_satker=:satker ORDER BY nama ASC");
+            $stmt->bindParam(':satker', $this->id_satker);
+            $stmt->execute();
+            $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $result = $stmt->fetchAll()[0]["COUNT(*)"];
+        
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+        return $result;
     }
     
     function check_92820()
     {
-        $this->db->from($this->table);
-        $this->db->join($this->autentifikasi, 'master_pegawai.niplama=autentifikasi.niplama');
-        $this->db->where('autentifikasi.id_level =', '3');
-        $this->db->where('id_org', '92820');
-        $this->db->where('master_pegawai.id_satker', $this->session->userdata('satker'));
-        $this->db->order_by("nama", "asc");
-        $query = $this->db->get();
+        // $this->db->from($this->table);
+        // $this->db->join($this->autentifikasi, 'master_pegawai.niplama=autentifikasi.niplama');
+        // $this->db->where('autentifikasi.id_level =', '3');
+        // $this->db->where('id_org', '92820');
+        // $this->db->where('master_pegawai.id_satker', $this->session->userdata('satker'));
+        // $this->db->order_by("nama", "asc");
+        // $query = $this->db->get();
     
-        return $query->num_rows();
+        // return $query->num_rows();
+
+        $result = "Error cek 92820";
+        try {
+            $conn = new PDO("mysql:host=$this->servername;dbname=$this->dbname_ckp", $this->username_ckp, $this->password_ckp);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $stmt = $conn->prepare("SELECT COUNT(*) FROM master_pegawai JOIN autentifikasi ON master_pegawai.niplama=autentifikasi.niplama WHERE autentifikasi.id_level=3
+                AND id_org='92820' AND master_pegawai.id_satker=:satker ORDER BY nama ASC");
+            $stmt->bindParam(':satker', $this->id_satker);
+            $stmt->execute();
+            $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $result = $stmt->fetchAll()[0]["COUNT(*)"];
+        
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+        return $result;
     }
     
     function check_92830()
     {
-        $this->db->from($this->table);
-        $this->db->join($this->autentifikasi, 'master_pegawai.niplama=autentifikasi.niplama');
-        $this->db->where('autentifikasi.id_level =', '3');
-        $this->db->where('id_org', '92830');
-        $this->db->where('master_pegawai.id_satker', $this->session->userdata('satker'));
-        $this->db->order_by("nama", "asc");
-        $query = $this->db->get();
+        // $this->db->from($this->table);
+        // $this->db->join($this->autentifikasi, 'master_pegawai.niplama=autentifikasi.niplama');
+        // $this->db->where('autentifikasi.id_level =', '3');
+        // $this->db->where('id_org', '92830');
+        // $this->db->where('master_pegawai.id_satker', $this->session->userdata('satker'));
+        // $this->db->order_by("nama", "asc");
+        // $query = $this->db->get();
     
-        return $query->num_rows();
+        // return $query->num_rows();
+
+        $result = "Error cek 92830";
+        try {
+            $conn = new PDO("mysql:host=$this->servername;dbname=$this->dbname_ckp", $this->username_ckp, $this->password_ckp);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $stmt = $conn->prepare("SELECT COUNT(*) FROM master_pegawai JOIN autentifikasi ON master_pegawai.niplama=autentifikasi.niplama WHERE autentifikasi.id_level=3
+                AND id_org='92830' AND master_pegawai.id_satker=:satker ORDER BY nama ASC");
+            $stmt->bindParam(':satker', $this->id_satker);
+            $stmt->execute();
+            $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $result = $stmt->fetchAll()[0]["COUNT(*)"];
+        
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+        return $result;
     }
     
     function check_92840()
     {
-        $this->db->from($this->table);
-        $this->db->join($this->autentifikasi, 'master_pegawai.niplama=autentifikasi.niplama');
-        $this->db->where('autentifikasi.id_level =', '3');
-        $this->db->where('id_org', '92840');
-        $this->db->where('master_pegawai.id_satker', $this->session->userdata('satker'));
-        $this->db->order_by("nama", "asc");
-        $query = $this->db->get();
+        // $this->db->from($this->table);
+        // $this->db->join($this->autentifikasi, 'master_pegawai.niplama=autentifikasi.niplama');
+        // $this->db->where('autentifikasi.id_level =', '3');
+        // $this->db->where('id_org', '92840');
+        // $this->db->where('master_pegawai.id_satker', $this->session->userdata('satker'));
+        // $this->db->order_by("nama", "asc");
+        // $query = $this->db->get();
     
-        return $query->num_rows();
+        // return $query->num_rows();
+
+        $result = "Error cek 92840";
+        try {
+            $conn = new PDO("mysql:host=$this->servername;dbname=$this->dbname_ckp", $this->username_ckp, $this->password_ckp);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $stmt = $conn->prepare("SELECT COUNT(*) FROM master_pegawai JOIN autentifikasi ON master_pegawai.niplama=autentifikasi.niplama WHERE autentifikasi.id_level=3
+                AND id_org='92840' AND master_pegawai.id_satker=:satker ORDER BY nama ASC");
+            $stmt->bindParam(':satker', $this->id_satker);
+            $stmt->execute();
+            $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $result = $stmt->fetchAll()[0]["COUNT(*)"];
+        
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+        return $result;
     }
     
     function check_92850()
     {
-        $this->db->from($this->table);
-        $this->db->join($this->autentifikasi, 'master_pegawai.niplama=autentifikasi.niplama');
-        $this->db->where('autentifikasi.id_level =', '3');
-        $this->db->where('id_org', '92850');
-        $this->db->where('master_pegawai.id_satker', $this->session->userdata('satker'));
-        $this->db->order_by("nama", "asc");
-        $query = $this->db->get();
+        // $this->db->from($this->table);
+        // $this->db->join($this->autentifikasi, 'master_pegawai.niplama=autentifikasi.niplama');
+        // $this->db->where('autentifikasi.id_level =', '3');
+        // $this->db->where('id_org', '92850');
+        // $this->db->where('master_pegawai.id_satker', $this->session->userdata('satker'));
+        // $this->db->order_by("nama", "asc");
+        // $query = $this->db->get();
     
-        return $query->num_rows();
+        // return $query->num_rows();
+
+        $result = "Error cek 92850";
+        try {
+            $conn = new PDO("mysql:host=$this->servername;dbname=$this->dbname_ckp", $this->username_ckp, $this->password_ckp);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $stmt = $conn->prepare("SELECT COUNT(*) FROM master_pegawai JOIN autentifikasi ON master_pegawai.niplama=autentifikasi.niplama WHERE autentifikasi.id_level=3
+                AND id_org='92850' AND master_pegawai.id_satker=:satker ORDER BY nama ASC");
+            $stmt->bindParam(':satker', $this->id_satker);
+            $stmt->execute();
+            $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $result = $stmt->fetchAll()[0]["COUNT(*)"];
+        
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+        return $result;
     }
     
     function check_92860()
     {
-        $this->db->from($this->table);
-        $this->db->join($this->autentifikasi, 'master_pegawai.niplama=autentifikasi.niplama');
-        $this->db->where('autentifikasi.id_level =', '3');
-        $this->db->where('master_pegawai.id_org', '92860');
-        $this->db->where('master_pegawai.id_satker', $this->session->userdata('satker'));
-        $this->db->order_by("nama", "asc");
-        $query = $this->db->get();
+        // $this->db->from($this->table);
+        // $this->db->join($this->autentifikasi, 'master_pegawai.niplama=autentifikasi.niplama');
+        // $this->db->where('autentifikasi.id_level =', '3');
+        // $this->db->where('master_pegawai.id_org', '92860');
+        // $this->db->where('master_pegawai.id_satker', $this->session->userdata('satker'));
+        // $this->db->order_by("nama", "asc");
+        // $query = $this->db->get();
     
-        return $query->num_rows();
+        // return $query->num_rows();
+
+        $result = "Error cek 92860";
+        try {
+            $conn = new PDO("mysql:host=$this->servername;dbname=$this->dbname_ckp", $this->username_ckp, $this->password_ckp);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $stmt = $conn->prepare("SELECT COUNT(*) FROM master_pegawai JOIN autentifikasi ON master_pegawai.niplama=autentifikasi.niplama WHERE autentifikasi.id_level=3
+                AND id_org='92860' AND master_pegawai.id_satker=:satker ORDER BY nama ASC");
+            $stmt->bindParam(':satker', $this->id_satker);
+            $stmt->execute();
+            $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $result = $stmt->fetchAll()[0]["COUNT(*)"];
+        
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+        return $result;
     }
     
     function get_pegawai_by_org_jab($id, $id_lvl)
     {
-        $this->db->from($this->table);
-        $this->db->join($this->autentifikasi, 'master_pegawai.niplama=autentifikasi.niplama');
-        $this->db->where('autentifikasi.id_level >',$id_lvl);
-        $this->db->where('id_org',$id);
-        $this->db->where('id_satker',$this->session->userdata('satker'));
-        $this->db->order_by("nama", "asc");
-        $query = $this->db->get();
+        // $this->db->from($this->table);
+        // $this->db->join($this->autentifikasi, 'master_pegawai.niplama=autentifikasi.niplama');
+        // $this->db->where('autentifikasi.id_level >',$id_lvl);
+        // $this->db->where('id_org',$id);
+        // $this->db->where('id_satker',$this->session->userdata('satker'));
+        // $this->db->order_by("nama", "asc");
+        // $query = $this->db->get();
     
-        return $query->result();
+        // return $query->result();
+
+        $result = "Error get pegawai by org jab";
+        try {
+            $conn = new PDO("mysql:host=$this->servername;dbname=$this->dbname_ckp", $this->username_ckp, $this->password_ckp);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $stmt = $conn->prepare("SELECT * FROM master_pegawai JOIN autentifikasi ON master_pegawai.niplama=autentifikasi.niplama WHERE autentifikasi.id_level>:id_lvl 
+                AND master_pegawai.id_satker=:satker AND master_pegawai.id_org=:id_org ORDER BY nama ASC");
+            $stmt->bindParam(':id_lvl', $id_lvl);
+            $stmt->bindParam(':id_org', $id);
+            $stmt->bindParam(':satker', $this->id_satker);
+            $stmt->execute();
+            $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $result = $stmt->fetchAll();
+            // $result = $stmt;
+        
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+        return $result;
     }
 
 }
 
 $pegawai = new Pegawai();
+$id_lvl = $_GET["id_lvl"];
+$id_satker = $_GET["id_satker"];
+$id_org = $_GET["id_org"];
 
-echo json_encode($pegawai->get_pegawai_es4());
-// $result = $pegawai->get_pegawai_es3();
-// echo $result;
+$pegawai->id_satker = $id_satker;
+$pegawai->id_org = $id_org;
 
+// print_r($_GET);
+// echo json_encode($pegawai->get_pegawai_es4_ksk());
 
+if (strcmp($id_lvl, '1') == 0) {
+    $result = $pegawai->get_pegawai_es3();
+} else if (strcmp($id_lvl, '2') == 0) {
+    if (strcmp(substr($id_satker, 2, 2), '00') == 0) {
+        $result = $pegawai->get_pegawai_es4();
+    } else {
+        $result = $pegawai->get_pegawai_es4_ksk();
+    }
+} else {
+    $result = $pegawai->get_pegawai_by_org_jab($id_org, $id_lvl);
+}
+
+echo json_encode($result);
+
+// http://localhost/pegawai.php?id_lvl=3&id_org=92860&id_satker=7471
 ?>
