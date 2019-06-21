bdf1e45508f0982b9320beae8f9e06b83191a947
wayan.saputra <wayan.saputra@bps.go.id>
2019-06-21 10:10:13 +0800

    add json unit kerja

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -3,6 +3,95 @@ export const url_refresh = "http://localhost/refresh.php";
 export const url_pegawai = "http://localhost/pegawai.php";
 export const url_login = "http://localhost/login.php";
 export const url_panduan = "http://localhost/panduan_apel_cakep.pdf";
+export const unit_kerja_prov = [
+    {
+        "id_org": 92000,
+        "nm_org": "BPS Propinsi",
+        "ket": 1
+    },
+    {
+        "id_org": 92100,
+        "nm_org": "Bagian Tata Usaha",
+        "ket": 1
+    },
+    {
+        "id_org": 92200,
+        "nm_org": "Bidang Statistik Sosial",
+        "ket": 1
+    },
+    {
+        "id_org": 92300,
+        "nm_org": "Bidang Statistik Produksi",
+        "ket": 1
+    },
+    {
+        "id_org": 92400,
+        "nm_org": "Bidang Statistik Distribusi",
+        "ket": 1
+    },
+    {
+        "id_org": 92500,
+        "nm_org": "Bidang Neraca Wilayah dan Analisis Statistik",
+        "ket": 1
+    },
+    {
+        "id_org": 92600,
+        "nm_org": "Bidang Integrasi Pengolahan dan Diseminasi Statistik",
+        "ket": 1
+    },
+    {
+        "id_org": 92800,
+        "nm_org": "BPS Kabupaten/Kota",
+        "ket": 2
+    }
+];
+export const unit_kerja_kab = [
+    {
+        "id_org": 92800,
+        "nm_org": "BPS Kabupaten/Kota",
+        "ket": 2
+    },
+    {
+        "id_org": 92810,
+        "nm_org": "Subbagian Tata Usaha",
+        "ket": 2
+    },
+    {
+        "id_org": 92820,
+        "nm_org": "Seksi Statistik Sosial",
+        "ket": 2
+    },
+    {
+        "id_org": 92830,
+        "nm_org": "Seksi Statistik Produksi",
+        "ket": 2
+    },
+    {
+        "id_org": 92840,
+        "nm_org": "Seksi Statistik Distribusi",
+        "ket": 2
+    },
+    {
+        "id_org": 92850,
+        "nm_org": "Seksi Neraca Wilayah dan Analisis Statistik",
+        "ket": 2
+    },
+    {
+        "id_org": 92860,
+        "nm_org": "Seksi Integrasi Pengolahan dan Diseminasi Statistik",
+        "ket": 2
+    },
+    {
+        "id_org": 92870,
+        "nm_org": "Koordinator Statistik Kecamatan",
+        "ket": 2
+    },
+    {
+        "id_org": 92880,
+        "nm_org": "Diperbantukan/Dipekerjakan",
+        "ket": 2
+    }
+];
 
 // const url_api = "http://10.74.8.176/api.php";
 // const url_refresh = "http://10.74.8.176/refresh.php";
